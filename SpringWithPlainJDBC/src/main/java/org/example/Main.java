b268b8894d251cc4f122f1757cc09a6aa2d36fa4
Patnana Sai Deekshith <patnanasaideekshith@Patnanas-MacBook-Pro.local>
2025-08-18 09:49:50 +0530

    Spring With Plain Jdbc

--- SpringWithPlainJDBC/src/main/java/org/example/Main.java
+++ SpringWithPlainJDBC/src/main/java/org/example/Main.java
@@ -8,36 +8,36 @@ public class Main {
         try {
             Connection conn = DriverManager.getConnection(URL);
 
-            Statement stmt = conn.createStatement();
+            Statement statement = conn.createStatement();
+//
+//             statement.executeUpdate("INSERT INTO student VALUES(1, 'Alice', 'New York')");
+//            ResultSet rs1 = statement.executeQuery("SELECT * FROM student");
+//            System.out.println("Statement Read:");
+//            while (rs1.next()) {
+//                System.out.println(rs1.getInt("id") + " " + rs1.getString("name") + " " + rs1.getString("city"));
+//            }
+//
+//             PreparedStatement preparedStatement = conn.prepareStatement(
+//                    "INSERT INTO student (id, name, city) VALUES (?, ?, ?)");
+//            preparedStatement.setInt(1, 2);
+//            preparedStatement.setString(2, "Bob");
+//            preparedStatement.setString(3, "Los Angeles");
+//            preparedStatement.executeUpdate();
+//
+//             PreparedStatement psSelect = conn.prepareStatement("SELECT * FROM student");
+//            ResultSet rs2 = psSelect.executeQuery();
+//            System.out.println("PreparedStatement Read:");
+//            while (rs2.next()) {
+//                System.out.println(rs2.getInt("id") + " " + rs2.getString("name") + " " + rs2.getString("city"));
+//            }
+//
+//            CallableStatement callableStatement = conn.prepareCall("{call add_student(?, ?, ?)}");
+//            callableStatement.setInt(1, 3);
+//            callableStatement.setString(2, "Charlie");
+//            callableStatement.setString(3, "Miami");
+//            callableStatement.execute();
 
-             stmt.executeUpdate("INSERT INTO student VALUES(1, 'Alice', 'New York')");
-            ResultSet rs1 = stmt.executeQuery("SELECT * FROM student");
-            System.out.println("Statement Read:");
-            while (rs1.next()) {
-                System.out.println(rs1.getInt("id") + " " + rs1.getString("name") + " " + rs1.getString("city"));
-            }
-
-             PreparedStatement psInsert = conn.prepareStatement(
-                    "INSERT INTO student (id, name, city) VALUES (?, ?, ?)");
-            psInsert.setInt(1, 2);
-            psInsert.setString(2, "Bob");
-            psInsert.setString(3, "Los Angeles");
-            psInsert.executeUpdate();
-
-             PreparedStatement psSelect = conn.prepareStatement("SELECT * FROM student");
-            ResultSet rs2 = psSelect.executeQuery();
-            System.out.println("PreparedStatement Read:");
-            while (rs2.next()) {
-                System.out.println(rs2.getInt("id") + " " + rs2.getString("name") + " " + rs2.getString("city"));
-            }
-
-            CallableStatement csInsert = conn.prepareCall("{call add_student(?, ?, ?)}");
-            csInsert.setInt(1, 3);
-            csInsert.setString(2, "Charlie");
-            csInsert.setString(3, "Miami");
-            csInsert.execute();
-
-            ResultSet rs3 = stmt.executeQuery("SELECT * FROM student");
+            ResultSet rs3 = statement.executeQuery("SELECT * FROM student");
             System.out.println("CallableStatement Read:");
             while (rs3.next()) {
                 System.out.println(rs3.getInt("id") + " " + rs3.getString("name") + " " + rs3.getString("city"));
